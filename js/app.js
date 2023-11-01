63bf6db0ad17712c8a73d2d466229232185981c1
Allan Robert Tan <argt@duck.com>
2023-11-01 15:33:38 +0800

    chore: change to window

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
   // Particles
   Particles.init({
     selector: '.particles-background',
