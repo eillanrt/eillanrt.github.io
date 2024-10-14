1592ad4691d5d85fbb5c471a44a041748aabcff4
Allan Robert Tan <argt@duck.com>
2024-10-14 10:06:03 +0800

    delete everythin: ready for reset

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import fetchLastUpdate from './fetch-last-update'
-import handleSubmitForm from './handle-submit-form'
-import validateForm from './validate-form'
-
-fetchLastUpdate()
-handleSubmitForm()
